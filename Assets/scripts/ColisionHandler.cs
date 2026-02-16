7fd7ff3933e93115147ea2046f7cd5f55b0020a7
Gods Will Khojee <ardiumwing@gmail.com>
2026-02-17 01:49:58 +0530

    player explosion

--- Assets/scripts/ColisionHandler.cs
+++ Assets/scripts/ColisionHandler.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class ColisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject playerDestroyed;
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(playerDestroyed, transform.position, Quaternion.identity);
         Debug.Log(other.gameObject.name);
+        Destroy(this.gameObject);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
