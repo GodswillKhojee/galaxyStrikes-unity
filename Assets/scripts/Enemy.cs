213fca858d59bdb86bf045c32835f15c38d9ff0a
Gods Will Khojee <ardiumwing@gmail.com>
2026-02-18 15:06:32 +0530

    added scoreBoard

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,9 +3,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
+    [SerializeField] int hitPoint = 5;
+    [SerializeField] int score = 10;
+    ScoreBoard sb;
+
+    private void Start()
+    {
+        sb = FindAnyObjectByType<ScoreBoard>();
+    }
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX,transform.position,Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoint--;
+        if (hitPoint <= 0)
+        {
+            sb.increaseScore(score);
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
