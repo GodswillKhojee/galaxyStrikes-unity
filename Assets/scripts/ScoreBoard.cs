96106691fc2b1136fc68cf880792ec061c10eae5
Gods Will Khojee <ardiumwing@gmail.com>
2026-02-24 20:46:48 +0530

    new enemies timeline

--- Assets/scripts/ScoreBoard.cs
+++ Assets/scripts/ScoreBoard.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score = 0;
+    [SerializeField] TMP_Text ScoreBoard_text;
 
     public void increaseScore(int amount)
     {
         score += amount;
         Debug.Log(score); 
+        ScoreBoard_text.text = score.ToString();
     }
 }
