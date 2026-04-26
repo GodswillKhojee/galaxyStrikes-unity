71b6122723100724aee4fd5c6c90b17d0f8e95b1
Gods Will Khojee <ardiumwing@gmail.com>
2026-04-26 13:26:24 +0530

    made scene reload when crashed

--- Assets/scripts/ColisionHandler.cs
+++ Assets/scripts/ColisionHandler.cs
@@ -3,21 +3,20 @@ using UnityEngine;
 public class ColisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject playerDestroyed;
+
+    // making a reference to a gamescenemanager
+
+    GameSceneManager gameSceneManager;
+    void Start()
+    {
+        gameSceneManager = FindAnyObjectByType<GameSceneManager>();   
+    }
     private void OnTriggerEnter(Collider other)
     {
+        gameSceneManager.ReloadLevel();
         Instantiate(playerDestroyed, transform.position, Quaternion.identity);
-        Debug.Log(other.gameObject.name);
+        //Debug.Log(other.gameObject.name);
         Destroy(this.gameObject);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
